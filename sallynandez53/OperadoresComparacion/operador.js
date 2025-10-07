791a6248230f45dbc53a30a93cfb8a1a92de96a0
sallynandez53 <sallynandez.53@gmail.com>
2025-10-07 13:57:31 -0600

    se agrego la pagina web

---
+++ sallynandez53/OperadoresComparacion/operador.js
@@ -0,0 +1,32 @@
+function calcular() {
+  const num1 = parseFloat(document.getElementById("num1").value);
+  const num2 = parseFloat(document.getElementById("num2").value);
+  const operador = document.getElementById("operador").value;
+  let resultado;
+
+  switch (operador) {
+    case ">":
+      resultado = num1 > num2;
+      break;
+    case "<":
+      resultado = num1 < num2;
+      break;
+    case ">=":
+      resultado = num1 >= num2;
+      break;
+    case "<=":
+      resultado = num1 <= num2;
+      break;
+    case "!=":
+    case "<>":
+      resultado = num1 != num2;
+      break;
+    case "=":
+      resultado = num1 == num2;
+      break;
+    default:
+      resultado = "Operador no válido";
+  }
+
+  document.getElementById("resultado").innerText = `Resultado: ${resultado}`;
+}
