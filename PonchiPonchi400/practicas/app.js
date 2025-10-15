27b234d0c97865a287f9101ffd89af088687dd53
Felipe <moningoe@gmail.com>
2025-10-15 17:08:17 -0600

    modify

--- PonchiPonchi400/practicas/app.js
+++ PonchiPonchi400/practicas/app.js
@@ -1,7 +1,13 @@
 const inputNombre = document.getElementById("nombre");
 const botonBuscar = document.getElementById("botonBuscar");
 
+async function buscarInfoPokemon(){
+   const respuesta = await fetch("https://pokeapi.co/api/v2/pokemon/" + inputNombre.value)
+   const infoPokemon = await respuesta.json();
+}
 botonBuscar.addEventListener("click", e => {
     e.preventDefault();
     console.log("listo")
+
+    buscarInfoPokemon();
 });
