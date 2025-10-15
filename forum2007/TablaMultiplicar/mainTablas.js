6acf3fd3bafac0ca0785b86eba8f53bf9e7c0213
Luis Antonio Escobedo Avila <luisantonioescobedoavila@gmail.com>
2025-10-14 20:11:55 -0600

    TablaMultiplicar

---
+++ forum2007/TablaMultiplicar/mainTablas.js
@@ -0,0 +1,50 @@
+const contenedor = document.getElementById("tabla");
+const btnGenerar = document.getElementById("btnGenerar");
+
+btnGenerar.addEventListener("click", e => {
+    
+    contenedor.innerHTML = "";
+
+    let numero = parseInt(document.getElementById("numero").value);
+    if (numero <= 0) {
+        alert("Ingresa un número válido");
+        return;
+    }
+
+    let tabla = document.createElement("table");
+    tabla.setAttribute("border", "1");
+    tabla.setAttribute("cellpadding", "5");
+    tabla.setAttribute("cellspacing", "0");
+
+    let filaEncabezado = document.createElement("tr");
+    let celdaX = document.createElement("td");
+    celdaX.textContent = "X";
+    filaEncabezado.appendChild(celdaX);
+
+    for (let i = 1; i <= numero; i++) {
+        let th = document.createElement("td");
+        th.textContent = i;
+        filaEncabezado.appendChild(th);
+    }
+
+    tabla.appendChild(filaEncabezado);
+
+    for (let i = 1; i <= numero; i++) {
+        let fila = document.createElement("tr");
+
+        let encabezado = document.createElement("td");
+        encabezado.textContent = i;
+        fila.appendChild(encabezado);
+
+        for (let j = 1; j <= numero; j++) {
+            let celda = document.createElement("td");
+            celda.textContent = i * j;
+            fila.appendChild(celda);
+        }
+
+        tabla.appendChild(fila);
+    }
+
+    contenedor.appendChild(tabla);
+});
+
