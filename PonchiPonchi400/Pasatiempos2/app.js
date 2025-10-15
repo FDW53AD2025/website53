aadd3cad8c32fcc8960673ff57729ba0e67ab713
Felipe <moningoe@gmail.com>
2025-10-15 17:03:56 -0600

    Actualizar

---
+++ PonchiPonchi400/Pasatiempos2/app.js
@@ -0,0 +1,10 @@
+let numero1;
+let numero2;
+let suma;
+
+numero1 = Number(prompt("Ingresa un numero"));
+numero2 = Number(prompt("Ingresa un numero"));
+
+suma = numero1 + numero2
+
+console.log('la suma es ${suma}');
---
+++ PonchiPonchi400/practicas/app.js
@@ -0,0 +1,7 @@
+const inputNombre = document.getElementById("nombre");
+const botonBuscar = document.getElementById("botonBuscar");
+
+botonBuscar.addEventListener("click", e => {
+    e.preventDefault();
+    console.log("listo")
+});
