9195b2c32f67a08f6a679d6181cf13f5df08c1f1
Felipe <moningoe@gmail.com>
2025-10-15 17:47:56 -0600

    .log

--- PonchiPonchi400/practicas/app.js
+++ PonchiPonchi400/practicas/app.js
@@ -3,6 +3,7 @@ const botonBuscar = document.getElementById("botonBuscar");
 const peso = document.getElementById("peso");
 const imagen = document.getElementById("imagen");
 const sonido = document.getElementById("sonido");
+const listaItems = document.getElementById("listaItems");
 
 async function buscarInfoPokemon(){
    const respuesta = await fetch("https://pokeapi.co/api/v2/pokemon/" + inputNombre.value)
@@ -10,6 +11,11 @@ async function buscarInfoPokemon(){
    peso.innerText = infoPokemon.weight;
    imagen.src = infoPokemon.sprites.other.home.front_default;
    sonido.src = infoPokemon.cries.latest;
+   infoPokemon.held_items.forEach(i => {
+    const itemLi = document.createElement("li");
+    itemLi.innerText = i.item.name;
+    
+   });
 
 }
 botonBuscar.addEventListener("click", e => {
